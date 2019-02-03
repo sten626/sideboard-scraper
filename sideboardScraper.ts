6cd22066de07fac68bcde499c36f0ef4a09e243c
Steven Indzeoski <steven.indzeoski@gmail.com>
2019-02-03 15:23:02 -0600

    Adding tslint.json and updating script slightly.

--- sideboardScraper.ts
+++ sideboardScraper.ts
@@ -1,7 +1,8 @@
 import * as cheerio from 'cheerio';
 import * as rp from 'request-promise';
 
-const url = 'https://magic.wizards.com/en/articles/archive/mtgo-standings/competitive-modern-constructed-league-2019-02-01'
+const url = 'https://magic.wizards.com/en/articles/archive/mtgo-standings/competitive-modern-constructed-league-'
+  + '2019-02-01';
 
 interface Card {
   name: string;
@@ -15,23 +16,23 @@ rp(url).then((html: string) => {
   $('.sorted-by-sideboard-container .row').each((i, card) => {
     const cardCount = parseInt($(card).find('.card-count').text());
     const cardName = $(card).find('.card-name a').text();
-    
+
     if (cardName in cards) {
       cards[cardName].count += cardCount;
     } else {
       cards[cardName] = {
-        name: cardName,
-        count: cardCount
+        count: cardCount,
+        name: cardName
       };
     }
   });
 
-  const cardsList = Object.keys(cards).map(name => cards[name]);
+  const cardsList = Object.keys(cards).map((name) => cards[name]);
   cardsList.sort((a: Card, b: Card) => {
     if (a.count === b.count) {
       return a.name.localeCompare(b.name);
     }
-    
+
     return b.count - a.count;
   });
 
